df48b29857dbee55207388d6de1817657cd63697
Alper <66705782+AlperDevelopment@users.noreply.github.com>
2023-05-16 21:34:44 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 CreateThread(function()
-    for i = 0, 1000 do 
+    for i = 0, 1000 do -- The npcs do not spawn in the dimensions 0-1000
         SetRoutingBucketPopulationEnabled(i, false)
     end
 end)
